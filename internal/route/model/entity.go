53b72e0a394e84e69555b4031ceaad46590ca8a5
Mikhail Epatko <m.epatko@jsa-group.ru>
2025-03-14 18:55:47 +0300

    tests for RouteRepository

--- internal/route/model/entity.go
+++ internal/route/model/entity.go
@@ -1,14 +1,14 @@
 package model
 
-import "approve/internal/common"
+import cm "approve/internal/common"
 
 type RouteEntity struct {
-	Id          int64         `db:"id"`
-	Name        string        `db:"name"`
-	Description string        `db:"description"`
-	Status      common.Status `db:"status"`
-	IsApproved  bool          `db:"is_approved"`
-	Deleted     bool          `db:"deleted"`
+	Id          int64     `db:"id"`
+	Name        string    `db:"name"`
+	Description string    `db:"description"`
+	Status      cm.Status `db:"status"`
+	IsApproved  bool      `db:"is_approved"`
+	Deleted     bool      `db:"deleted"`
 }
 
 func (e RouteEntity) ToResponse() RouteResponse {
